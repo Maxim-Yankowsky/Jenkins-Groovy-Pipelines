8c1b2a70b422c56baf6ae7eacdb82c354a02f43a
Maxim Yankowsky <68082132+Maxim-Yankowsky@users.noreply.github.com>
2020-10-21 22:48:02 +0300

    Update Simple-pipeline.groovy

--- Simple-pipeline.groovy
+++ Simple-pipeline.groovy
@@ -2,21 +2,21 @@ pipeline {
     agent { label 'Slave-1'}
 
     stages {
-        stage('Nemnojko teks po dibilnomy napisan') {
+        stage('Create') {
             steps {
                 echo "Dubl-1"
                 sh "mkdir test"
                 sh "touch test-file"
             }
         }
-        stage('do sih por nihuya ne ponjal') {
+        stage('rules') {
             steps {
                 echo "ne pon"
                 sh "chmod 700 test-file"
                 echo "rabotaet blet"
             }
         }
-        stage('nu bolee menee ponjal') {
+        stage('final') {
             steps {
                 sh "rm -rf test"
                 sh "chmod 600 test-file"
